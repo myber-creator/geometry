7819c357c91e2109251448e86834607e2f710b74
Maksim <jeffrimorgan@yandex.ru>
2020-02-18 15:57:26 +0700

    add function print

--- circle.c
+++ circle.c
@@ -8,7 +8,22 @@ struct Point {
 struct Point circle[100];
 struct Point triangle[100];
 
-
+int print(struct Point circle[100], struct Point triangle[100])
+{
+	int i;
+	printf("circle(");
+	for(i = 0; i < 2; ++i){
+		printf("%d %d", circle[i].x, circle[i].y);
+		printf(",");
+	}
+	printf(")\n");
+	printf("triangle(");
+	for(i = 0; i < 3; ++i){
+		printf("%d %d", triangle[i].x, triangle[i].y);
+		printf(",");
+	}
+	printf(")\n");
+}
 
 int main()
 {
