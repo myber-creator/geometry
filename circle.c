4f97a58be1fa5c7e1efed0c0c38888db012d57b5
Maksim <jeffrimorgan@yandex.ru>
2020-02-25 16:20:17 +0700

    edit gitignore and add full circle

--- circle.c
+++ circle.c
@@ -1,40 +1,36 @@
 #include <stdio.h>
 
-struct Point {
-    int x;
-    int y;
+struct figure {
+    char name[10];
+    int param[3];
 };
 
-struct Point circle[100];
-struct Point triangle[100];
-
-int print(struct Point circle[100], struct Point triangle[100])
+void read_file(FILE* file, int n, struct figure circle[n])
 {
-	int i;
-	printf("circle(");
-	for(i = 0; i < 2; ++i){
-		printf("%d %d", circle[i].x, circle[i].y);
-		printf(",");
-	}
-	printf(")\n");
-	printf("triangle(");
-	for(i = 0; i < 3; ++i){
-		printf("%d %d", triangle[i].x, triangle[i].y);
-		printf(",");
-	}
-	printf(")\n");
+    int i;
+    for (i = 0; i < n; i++) {
+        fscanf(file,
+               "%s (%d %d, %d)",
+               circle[i].name,
+               &circle[i].param[0],
+               &circle[i].param[1],
+               &circle[i].param[2]);
+        printf("%s %d (%d %d, %d)\n",
+               circle[i].name,
+               i + 1,
+               circle[i].param[0],
+               circle[i].param[1],
+               circle[i].param[2]);
+    }
 }
-
 int main()
 {
-	int i;
-	printf("Введите точку окружности:\n");
-	for(i = 0; i < 2; i++){
-		scanf("%d %d", &circle[i].x, &circle[i].y);
-	}
-	printf("Введите точки треугольника:\n");
-	for(i = 0; i < 3; i++){
-		scanf("%d %d", &triangle[i].x, &triangle[i].y);
-	}
-	print(circle,triangle);
+    FILE* file;
+    int n;
+    printf("Enter n circle:\n");
+    scanf("%d", &n);
+    struct figure circle[n];
+    file = fopen("figures.txt", "r");
+    read_file(file, n, circle);
+    return 0;
 }
