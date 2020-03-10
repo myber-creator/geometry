6fc8f7678ae857125a90d31a9d4c4d2e2eae7ae1
Maksim <jeffrimorgan@yandex.ru>
2020-03-10 15:50:45 +0700

    Return version

--- circle.c
+++ circle.c
@@ -8,13 +8,13 @@ struct figure {
 void read_file(FILE* file, int n, struct figure circle[n])
 {
     int i;
-    while (fscanf(file,
-                  "%s (%d %d, %d)",
-                  circle[i].name,
-                  &circle[i].param[0],
-                  &circle[i].param[1],
-                  &circle[i].param[2]);
-           != EOF) {
+    for (i = 0; i < n; i++) {
+        fscanf(file,
+               "%s (%d %d, %d)",
+               circle[i].name,
+               &circle[i].param[0],
+               &circle[i].param[1],
+               &circle[i].param[2]);
         printf("%s %d (%d %d, %d)\n",
                circle[i].name,
                i + 1,
@@ -26,7 +26,9 @@ void read_file(FILE* file, int n, struct figure circle[n])
 int main()
 {
     FILE* file;
-    struct figure circle[10];
+	int n;
+	scanf("%d", &n);
+    struct figure circle[n];
     file = fopen("figures.txt", "r");
     read_file(file, n, circle);
     return 0;
